85cd8c78c9234b11be22b10c0968ef0a900e0323
Denzel digital <j9dagi2025@gmail.com>
2026-04-18 21:46:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -195,8 +195,9 @@ def buy(c):
 
     
     pay_url = f"https://j9dagi2025-stack.github.io/?am={price}"
+
     kb = InlineKeyboardMarkup()
-    kb.add(InlineKeyboardButton("🚀 PAY NOW",url="https://j9dagi2025-stack.github.io/index.html/"))
+    kb.add(InlineKeyboardButton("🚀 PAY NOW", url=pay_url))
     kb.add(InlineKeyboardButton("💳 I HAVE PAID", callback_data="paid"))
     kb.add(InlineKeyboardButton("❌ CANCEL ORDER", callback_data="cancel"))
 
